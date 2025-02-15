817ba2615e98fda588c69e4fb7a3ed330dd2192a
danielaser <daniela.serrano.dev@gmail.com>
2025-02-15 17:19:53 -0500

    chore: project setup generic domain

---
+++ src/shared/src/domain/src/main/java/com/buildingblocks/shared/domain/generic/DomainActionsHandler.java
@@ -0,0 +1,40 @@
+package com.buildingblocks.shared.domain.generic;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Consumer;
+
+public class DomainActionsHandler {
+    private final List<DomainEvent> events = new LinkedList<>();
+    private final Map<String, AtomicLong> versions = new ConcurrentHashMap<>();
+    private final Set<Consumer<? super DomainEvent>> actions = new HashSet<>();
+
+    public List<DomainEvent> getEvents() {
+        return events;
+    }
+
+    public void subscribe(final DomainActionsContainer actionsContainer) {
+        actions.addAll(actionsContainer.actions);
+    }
+
+    public void apply(final DomainEvent event) {
+        events.add(event);
+        actions.forEach(action -> handle(event, action));
+    }
+
+    private void handle(final DomainEvent event, final Consumer<? super DomainEvent> consumer) {
+        try {
+           consumer.accept(event);
+           final long newVersion = increaseVersion(event);
+           event.setVersion(newVersion);
+        } catch (ClassCastException ignored){ }
+    }
+
+    private long increaseVersion(final DomainEvent event) {
+        final AtomicLong version = versions.get(event.getName());
+        final long newVersion = version == null ? event.getVersion() : version.incrementAndGet();
+        versions.put(event.getName(), new AtomicLong(newVersion));
+        return newVersion;
+    }
+}
