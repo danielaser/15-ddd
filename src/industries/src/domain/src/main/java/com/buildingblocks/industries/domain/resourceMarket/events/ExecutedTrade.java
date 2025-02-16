cb318f94f087f517b8f9fac004eacc836f399d09
danielaser <daniela.serrano.dev@gmail.com>
2025-02-15 21:29:03 -0500

    feat(develop): add atributes to resourceMarket events}

--- src/industries/src/domain/src/main/java/com/buildingblocks/industries/domain/resourceMarket/events/ExecutedTrade.java
+++ src/industries/src/domain/src/main/java/com/buildingblocks/industries/domain/resourceMarket/events/ExecutedTrade.java
@@ -3,8 +3,38 @@ package com.buildingblocks.industries.domain.resourceMarket.events;
 import com.buildingblocks.shared.domain.generic.DomainEvent;
 
 public class ExecutedTrade extends DomainEvent {
+    private final String id;
+    private final String tradeType;
+    private final String resourceType;
+    private final Integer totalResourcesPrice;
+    private final Integer resourceQuantity;
 
-    public ExecutedTrade() {
+    public ExecutedTrade(String id, String tradeType, String resourceType, Integer totalResourcesPrice, Integer resourceQuantity) {
         super(EventsEnum.EXECUTED_TRADE.name());
+        this.id = id;
+        this.tradeType = tradeType;
+        this.resourceType = resourceType;
+        this.totalResourcesPrice = totalResourcesPrice;
+        this.resourceQuantity = resourceQuantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTradeType() {
+        return tradeType;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public Integer getTotalResourcesPrice() {
+        return totalResourcesPrice;
+    }
+
+    public Integer getResourceQuantity() {
+        return resourceQuantity;
     }
 }
--- src/industries/src/domain/src/main/java/com/buildingblocks/industries/domain/resourceMarket/events/RefilledMarketSupply.java
+++ src/industries/src/domain/src/main/java/com/buildingblocks/industries/domain/resourceMarket/events/RefilledMarketSupply.java
@@ -3,8 +3,38 @@ package com.buildingblocks.industries.domain.resourceMarket.events;
 import com.buildingblocks.shared.domain.generic.DomainEvent;
 
 public class RefilledMarketSupply extends DomainEvent {
+    private final String id;
+    private final String resourceType;
+    private final Integer addedResourceQuantity;
+    private final Integer updatedAvailableResources;
+    private final Integer updatedResourcePrice;
 
-    public RefilledMarketSupply() {
+    public RefilledMarketSupply(String id, String resourceType, Integer addedResourceQuantity, Integer updatedAvailableResources, Integer updatedResourcePrice) {
         super(EventsEnum.REFILLED_MARKET_SUPPLY.name());
+        this.id = id;
+        this.resourceType = resourceType;
+        this.addedResourceQuantity = addedResourceQuantity;
+        this.updatedAvailableResources = updatedAvailableResources;
+        this.updatedResourcePrice = updatedResourcePrice;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getResourceType() {
+        return resourceType;
+    }
+
+    public Integer getAddedResourceQuantity() {
+        return addedResourceQuantity;
+    }
+
+    public Integer getUpdatedResourcePrice() {
+        return updatedResourcePrice;
+    }
+
+    public Integer getUpdatedAvailableResources() {
+        return updatedAvailableResources;
     }
 }
